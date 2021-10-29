0a91c76259ee6d3219b172b17499260234067fd6
An Dyk Bui <andyk.bui@plus4u.net>
2021-10-29 20:56:47 +0300

    insert variable in template

--- src/components/EmailTemplate.vue
+++ src/components/EmailTemplate.vue
@@ -2,22 +2,46 @@
   <div class="col-span-4">
     <div class="text-left">
       <span>Insert token:</span>
-      <button class="bg-green-800 text-gray-100 p-1" @click="handleClick('first_name')">name</button>
-      <button class="bg-green-800 text-gray-100 p-1" @click="handleClick('email')">email</button>
-      <button class="bg-green-800 text-gray-100 p-1" @click="handleClick('phone_number')">phone</button>
-      <button class="bg-green-800 text-gray-100 p-1" @click="handleClick('website')">website</button>
+      <button class="bg-green-600 text-gray-100 p-1" @click="handleClick('first_name')">name</button>
+      <button class="bg-green-600 text-gray-100 p-1" @click="handleClick('email')">email</button>
+      <button class="bg-green-600 text-gray-100 p-1" @click="handleClick('phone_number')">phone</button>
+      <button class="bg-green-600 text-gray-100 p-1" @click="handleClick('website')">website</button>
     </div>
-    <textarea class="border-2 border-black rounded-sm h-full w-full p-3 mt-2.5"></textarea>
+    <div
+      class="border-2 border-black rounded-sm h-full w-full p-3 mt-2.5 text-left content"
+      contenteditable="true"
+      v-html="html"
+      ref="input"
+      @input="handleInput($event)"
+    ></div>
+    <show-input :text="textToDisplay" />
   </div>
 </template>
 <script>
 import { mapGetters } from "vuex";
+import ShowInput from "./ShowInput";
 
 export default {
   name: "EmailTemplate",
+  components: {
+    ShowInput,
+  },
+  data() {
+    return {
+      html: "",
+      textToDisplay: "",
+    };
+  },
   methods: {
     handleClick(key) {
-      console.log(this.getUserAttribute(key));
+      const token = this.getUserAttribute(key);
+      const contentOfInput = this.$refs.input.innerHTML;
+      const tokenTemplate = `&nbsp<span class="inline-block border-2 border-dashed border-green-700 p-1 mb-3" contenteditable="false">${token}</span>&nbsp`;
+      this.html = contentOfInput + tokenTemplate;
+      this.textToDisplay = this.html;
+    },
+    handleInput() {
+      this.textToDisplay = this.$refs.input.innerHTML;
     },
   },
   computed: {
@@ -35,5 +59,12 @@ button {
 }
 textarea {
   resize: none;
+  background-color: #fafaf8;
+}
+.highlighted {
+  display: inline-block;
+  border: 2px dashed #81bbab;
+  padding: 1px;
+  margin-bottom: 10px;
 }
 </style>
---
+++ src/components/ShowInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-span-4 mt-3">
+    <div class="text-left">
+      <button class="bg-blue-600 text-gray-100 p-1">save to localStorate</button>
+      <button class="bg-blue-600 text-gray-100 p-1 ml-3">insert from localStorate</button>
+    </div>
+    <div class="border-2 border-black rounded-sm  w-full p-3 mt-2.5 text-left content " v-html="text"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "showInput",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+
+  watch: {},
+  computed: {
+    parseText() {
+      return this.text.replace(/<[^>]+>/g, "").replace(/&nbsp;/g, " ");
+    },
+  },
+};
+</script>
+<style lang="scss"></style>
